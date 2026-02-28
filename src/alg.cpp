79f40af85a2879e6ea286a67b5153510b82e312e
VarvaraSed <varvarasedyshevafgh@gmail.com>
2026-02-28 23:40:34 +0300

    upd-again-dz

--- src/alg.cpp
+++ src/alg.cpp
@@ -10,11 +10,9 @@ bool checkPrime(uint64_t value) {
 	if (value <= 3) {
 		return true;
 	}
-
 	if (value % 2 == 0 || value % 3 == 0) {
 		return false;
 	}
-
 	for (uint64_t i = 5; i * i <= value; i += 6) {
 		if (value % i == 0 || value % (i + 2) == 0) {
 			return false;
@@ -30,7 +28,6 @@ uint64_t nPrime(uint64_t n) {
 	if (n == 1) {
 		return 2;
 	}
-
 	uint64_t count = 1;
 	uint64_t secondPerem = 3;
 	while (count < n) {
@@ -60,7 +57,6 @@ uint64_t sumPrime(uint64_t hbound) {
 	if (hbound <= 2) {
 		return 0;
 	}
-
 	uint64_t sum = 2;
 	for (uint64_t i = 3; i < hbound; i += 2) {
 		if (checkPrime(i)) {
@@ -73,7 +69,6 @@ uint64_t sumPrime(uint64_t hbound) {
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
 	uint64_t count = 0;
 	uint64_t start = (lbound < 3) ? 3 : lbound;
-
 	for (uint64_t i = start; i + 2 < hbound; i++) {
 		if (checkPrime(i) && checkPrime(i + 2)) {
 			count++;
