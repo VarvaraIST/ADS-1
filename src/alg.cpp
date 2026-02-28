bfc3b6de1d0979850017b4441f967e48bb283758
VarvaraSed <varvarasedyshevafgh@gmail.com>
2026-02-28 23:08:33 +0300

    update ADS1-Dz

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,34 @@
 
 
 bool checkPrime(uint64_t value) {
-  if (value <= 1) return false;
-  if (value <= 3) return true;
-
-  if (value % 2 == 0 || value % 3 == 0) return false;
-
+  if (value <= 1) {
+    return false;
+  }
+  if (value <= 3) {
+    return true;
+  }
+  if (value % 2 == 0 || value % 3 == 0) {
+    return false;
+  } 
+  
   for (uint64_t i = 5; i * i <= value; i += 6) {
     if (value % i == 0 || value % (i + 2) == 0) {
       return false;
-    }
+      }
   }
   return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-  if (n == 1) return 2;
-
+  if (n == 0) {
+    return 0;
+  }
+  if (n == 1) {
+    return 2;
+  }
+  
   uint64_t count = 1;
-  uint64_t secondPerem = 3;  
-
+  uint64_t secondPerem = 3;
   while (count < n) {
     if (checkPrime(secondPerem)) {
       count++;
@@ -41,17 +50,19 @@ uint64_t nextPrime(uint64_t value) {
   while (!checkPrime(candidate)) {
     candidate++;
   }
-  return candidate;
+  return candidate - 2;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  if (hbound <= 2) return 0;
+  if (hbound <= 2) {
+    return 0;
+  }
 
   uint64_t sum = 2;
   for (uint64_t i = 3; i < hbound; i += 2) {
     if (checkPrime(i)) {
       sum += i;
-      }
+    }
   }
   return sum;
 }
