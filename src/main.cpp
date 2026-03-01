254733e0ed0e31b6cb2b3b6cd3b26e4af72e5024
VarvaraSed <varvarasedyshevafgh@gmail.com>
2026-03-01 18:00:28 +0300

    edit-and-upd-main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,24 @@
 // Copyright 2022 NNTU-CS
+#include <iostream>
 #include "alg.h"
 
 int main() {
-    return 0;
+    uint64_t num = 19;
+    if (checkPrime(num)) {
+        std::cout << num << " - prime number\n";
+    }
+    else {
+        std::cout << num << " - not prime\n";
+    }
+    // Проверка метода nPrime
+    std::cout << "5th prime number: " << nPrime(5) << std::endl;
+
+    // Проверка метода nextPrime
+    std::cout << "Next prime number after 20: " << nextPrime(20) << std::endl;
+
+    // Проверка метода sumPrime
+    std::cout << "Sum of prime numbers up to 10: " << sumPrime(10) << std::endl;
+
+    // Проверка метода twinPrimes
+    std::cout << "Pairs of twins between 1 and 20: " << twinPrimes(1, 20) << std::endl;
 }
